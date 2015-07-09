2e4ff0eaf610692e1f1881d35b616f46be061a0a
stevelowenthal <steve@datastax.com>
2015-07-09 12:54:25 +0100

    use broadcast address instead of multiregion snitch

--- src/lab16/phase6.sh
+++ src/lab16/phase6.sh
@@ -2,7 +2,7 @@
 
 set -v
 
-ssh node5_ext "sed '/^endpoint_snitch/ cendpoint_snitch: Ec2Snitch' -i /etc/cassandra/cassandra.yaml"
+ssh node5_ext "sed '/^broadcast_address/ d'
 
 ssh node5_ext service cassandra restart
 
