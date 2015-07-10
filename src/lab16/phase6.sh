334735c569700148c9b359ea27c18ed5127287f2
stevelowenthal <steve@datastax.com>
2015-07-10 09:34:42 +0100

    fix broadcast address

--- src/lab16/phase6.sh
+++ src/lab16/phase6.sh
@@ -2,7 +2,7 @@
 
 set -v
 
-ssh node5_ext "sed '/^broadcast_address/ d'
+ssh node5_ext "sed 's/^broadcast_address/#&/' -i /etc/cassandra/cassandra.yaml "
 
 ssh node5_ext service cassandra restart
 
